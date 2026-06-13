3b456835cbf459fdc7e946bb02beb1e59b78d577
Music Recommendation System <suhaniagghi09@gmail.com>
2026-06-14 01:07:27 +0530

    testing tasks

---
+++ test_task.py
@@ -0,0 +1,7 @@
+# test_tasks.py
+
+from tasks.email_task import email_task
+from tasks.inventory_task import inventory_task
+from tasks.manager_task import manager_task
+
+print("Tasks loaded successfully")
