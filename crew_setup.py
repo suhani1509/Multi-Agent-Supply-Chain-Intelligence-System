fedf063d395f7b4f1a29102995f2c630d4e3d3ae
Music Recommendation System <suhaniagghi09@gmail.com>
2026-07-06 00:43:12 +0530

    setup for report agent

--- crew_setup.py
+++ crew_setup.py
@@ -1,8 +1,10 @@
 from crewai import Crew
 
+from agents import report_agent
 from agents.email_agent import email_agent
 from agents.inventory_agent import inventory_agent
 from agents.manager_agent import manager_agent
+from tasks import report_task
 
 from tasks.email_task import email_task
 from tasks.inventory_task import inventory_task
@@ -12,12 +14,14 @@ supply_chain_crew = Crew(
     agents=[
         email_agent,
         inventory_agent,
-        manager_agent
+        manager_agent,
+        report_agent
     ],
     tasks=[
         email_task,
         inventory_task,
-        manager_task
+        manager_task,
+        report_task
     ],
     verbose=True
 )
