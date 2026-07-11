de28443ff1eeadf1387fd06a570d2cc9ee1c1227
Music Recommendation System <suhaniagghi09@gmail.com>
2026-07-11 22:16:09 +0530

    generating pdf

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from crew_setup import supply_chain_crew
+from reports.report_generator import generate_pdf
 
 result = supply_chain_crew.kickoff()
 
-print("\nFINAL RESULT:\n")
-print(result)
+email_report = result.tasks_output[0].raw
+inventory_report = result.tasks_output[1].raw
+manager_report = result.tasks_output[2].raw
+
+pdf_path = generate_pdf(
+    email_report,
+    inventory_report,
+    manager_report
+)
+
+print("\nPDF Generated Successfully!")
+print(pdf_path)
