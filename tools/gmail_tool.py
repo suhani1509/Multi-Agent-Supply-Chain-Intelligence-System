4b2fecf979711bf3653114c29045ab141fefcfea
Music Recommendation System <suhaniagghi09@gmail.com>
2026-07-16 17:13:27 +0530

    creating secrets.toml

--- tools/gmail_tool.py
+++ tools/gmail_tool.py
@@ -1,42 +1,46 @@
+import json
+import streamlit as st
+
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-import os
 
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly"
+]
+print(type(st.secrets["GOOGLE_TOKEN"]))
 
+json.loads(
+    st.secrets["GOOGLE_TOKEN"]
+)
+
+print("JSON loaded successfully")
 
 def authenticate_gmail():
 
-    creds = None
+    token_info = json.loads(
+        st.secrets["GOOGLE_TOKEN"]
+    )
 
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file(
-            "token.json",
-            SCOPES
+    creds = Credentials.from_authorized_user_info(
+        token_info,
+        SCOPES
+    )
+
+    if creds.expired and creds.refresh_token:
+
+        creds.refresh(
+            Request()
         )
 
-    if not creds or not creds.valid:
-
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials/credentials.json",
-                SCOPES
-            )
-
-            creds = flow.run_local_server(port=0)
-
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-
     service = build(
+
         "gmail",
+
         "v1",
+
         credentials=creds
+
     )
 
     return service
